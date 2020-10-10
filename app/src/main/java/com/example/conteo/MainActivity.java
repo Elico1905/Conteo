e4b8eb609e67e730eb939d96b10d85500751c336
Elico1905 <elico1904@hotmail.com>
2020-10-10 13:12:17 -0500

    ya funciona la aplicacion con 2 botones

---
+++ app/src/main/java/com/example/conteo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.conteo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.skydoves.elasticviews.ElasticImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ElasticImageView boton,boton2;
+    TextView caja;
+    int c=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boton=(ElasticImageView)findViewById(R.id.boton);
+        boton2=(ElasticImageView)findViewById(R.id.boton2);
+        caja=(TextView)findViewById(R.id.caja);
+        leer();
+        caja.setText(c+"");
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c++;
+                caja.setText(c+"");
+                escribir();
+            }
+        });
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limpiar();
+            }
+        });
+    }
+
+
+    public void leer(){
+        SharedPreferences preferences = getSharedPreferences("diario", Context.MODE_PRIVATE);
+        c = preferences.getInt("c",0);
+    }
+
+    public void escribir(){
+        SharedPreferences preferences = getSharedPreferences("diario", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("c",c);
+        editor.commit();
+    }
+
+
+    public void limpiar(){
+        SharedPreferences preferences = getSharedPreferences("diario", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.commit();
+        c = 0;
+        caja.setText("0");
+    }
+}
