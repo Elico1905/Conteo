b8094ce4201b9baacb028e8fe190986d1578f711
Elico1905 <elico1904@hotmail.com>
2020-10-10 13:27:29 -0500

    first commit

--- app/src/main/java/com/example/conteo/MainActivity.java
+++ app/src/main/java/com/example/conteo/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     TextView caja;
     int c=0;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
